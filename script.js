fd37c58606d8617fdf6e34f1e6fa81d8054febac
Navin Jethwani <106697006+Navin-Jethwani-76@users.noreply.github.com>
2023-02-28 11:35:26 +0530

    DAY_3

--- script.js
+++ script.js
@@ -1,30 +1,58 @@
-let isDrawing = false;
-let drawingBoard = document.getElementById("drawing-board");
+const board = document.querySelector("svg");
+const mode = document.getElementById("mode-select");
+const boardText = document.getElementById("text");
+var isPainting = false;
+var isErasing = false;
 
-drawingBoard.addEventListener("mousedown", startDraw);
-drawingBoard.addEventListener("mouseup", stopDraw);
-drawingBoard.addEventListener("mousemove", draw);
+board.addEventListener("mousedown", startDraw);
+board.addEventListener("mouseup", stopDraw);
+board.addEventListener("mousemove", draw);
+mode.addEventListener('change', handleModeChange);
+
+const svgRect = board.getBoundingClientRect();
 
 function startDraw(e) {
-  isDrawing = true;
-  const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
-  line.setAttribute("x1", e.clientX);
-  line.setAttribute("y1", e.clientY);
-  line.setAttribute("x2", e.clientX);
-  line.setAttribute("y2", e.clientY);
-  line.setAttribute("stroke", "#000");
-  line.setAttribute("stroke-width", "3");
-  drawingBoard.appendChild(line);
+  if(mode.value === 'pencil'){
+    isPainting = true;
+    isErasing = false;
+    const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    line.setAttribute('x1', e.clientX - svgRect.left);
+    line.setAttribute('y1', e.clientY - svgRect.top);
+    line.setAttribute('x2', e.clientX - svgRect.left);
+    line.setAttribute('y2', e.clientY - svgRect.top);
+    line.setAttribute('style', "stroke:rgb(0,0,0);stroke-width:2")
+    board.appendChild(line);
+}
+else if(mode.value === 'eraser'){
+    isErasing = true;
+    isPainting = false;
+    const element = document.elementFromPoint(e.clientX, e.clientY);
+    if(element.tagName === 'line'){
+        element.remove();
+    }
+}
 }
 
 function draw(e) {
-  if (!isDrawing) return;
-  const line = drawingBoard.lastChild;
-  line.setAttribute("x2", e.clientX);
-  line.setAttribute("y2", e.clientY);
+  if(isPainting === false) return;
+    else if(isPainting === true){
+        boardText.setAttribute('style', 'display: none;');
+        const line = board.lastChild;
+        line.setAttribute('x2', e.clientX - svgRect.left);
+        line.setAttribute('y2', e.clientY - svgRect.top);
+    }
 }
 
 function stopDraw(e) {
-  isDrawing = false;
+  isPainting = false;
+  isErasing = false;
+  
+  if(!board.hasChildNodes()){
+      boardText.setAttribute('style', '');
+  }
 }
-    
+
+function handleModeChange() {
+  isDrawing = false;
+  isErasing = false;
+}
